1df6fcc9a2b8b2583572828e9dd7d4aa433a747d
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2022-11-26 12:38:09 -0300

    remove invoice param

--- cap01/principal.js
+++ cap01/principal.js
@@ -2,29 +2,32 @@ const invoice = require('./invoices.json');
 const plays = require('./plays.json');
 
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
-    for (let perf of invoice.performances) {
+function renderPlainText(data, plays) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
     }
 
-    function totalVolumeCredits(invoice) {
+    function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
         return result;
