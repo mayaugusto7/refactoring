80596309efe8a0c4d93d738cc30dc155275124be
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2022-11-28 21:25:31 -0300

    implementation performance calculator

--- cap01/createStatementData.js
+++ cap01/createStatementData.js
@@ -1,3 +1,5 @@
+import {PerformanceCalculator} from "./performanceCalculator.js";
+
 export default function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -7,8 +9,9 @@ export default function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(aPerformance);
+        result.play = calculator.play;
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
         return result;
---
+++ cap01/performanceCalculator.js
@@ -0,0 +1,6 @@
+export class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+}
